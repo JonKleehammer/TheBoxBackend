78bd24262139f3f38b7ad085122313026da505d9
jon <jon.kleehammer@lease-analytics.com>
2023-05-29 19:58:20 -0400

    got the player list updating in real time as players join and leave through ActionCable
    Right now we're storing session data in memory via a global variable for the lobby, which is definitely gross.
    Long term, will probably need to look into solutions like actually working with the database, but if the project is small enough we may not need it

--- app/channels/lobby_channel.rb
+++ app/channels/lobby_channel.rb
@@ -1,14 +1,40 @@
 class LobbyChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "lobby_channel"
     puts "CLIENT CONNECTED"
+    @lobby_id = "LobbyChannel-#{params[:lobby_code]}"
+    stream_from @lobby_id
+
+    ActionCable.server.broadcast(@lobby_id, "New User Joined: #{params[:user_id]}")
+    user_date = params.slice(:user_id, :username).to_h
+    add_user_to_lobby(user_date)
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
     puts "CLIENT DISCONNECTED"
+    remove_user_from_lobby(params[:user_id])
   end
 
-  def test_message(payload)
+  private
+
+  def lobby_users
+    @@lobby_users ||= {}
+  end
+
+  def add_user_to_lobby(user)
+    puts "ADD #{user} to lobby"
+    lobby_users[@lobby_id] ||= []
+    lobby_users[@lobby_id] << user
+    broadcast_player_list
+  end
+
+  def remove_user_from_lobby(user_id)
+    lobby_users[@lobby_id].reject! { |user| user['user_id'] === user_id } if lobby_users[@lobby_id]
+    broadcast_player_list
+  end
+
+  def broadcast_player_list
+    players = lobby_users[@lobby_id] || []
+    ActionCable.server.broadcast(@lobby_id, { action: 'UPDATE_PLAYERS', payload: players })
   end
 end
